a0637ca667d852cfefa6088e6ae023f15aa76c31
Andrew Niemantsverdriet <andrewniemants@gmail.com>
2017-04-11 11:33:34 -0600

    Initial check-in

---
+++ public/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        googleCalendarApiKey: 'AIzaSyAMWvTcbCbJpLEyFtCWcDwbyIpcdIaDciw',
+        defaultView: 'agendaWeek',
+        scrollTime: '09:00:00',
+        eventSources: [
+          {
+              googleCalendarId: 'rocky.edu_sakv6bmfg59lf2dp3a8ogj1608@group.calendar.google.com',
+              className: 'rmc-student-events'
+          },
+          {
+              googleCalendarId: 'rocky.edu_2gbenc7go3t1k90k0m3utq7g4o@group.calendar.google.com',
+              className: 'master-calendar'
+          },
+          {
+              googleCalendarId: 'rocky.edu_4v1r3jp4eqa21812fbmknahisg@group.calendar.google.com',
+              className: 'rmc-sports'
+          },
+          {
+              googleCalendarId: 'rocky.edu_mhe9lkvurr3hl4teq1opoi9mdc@group.calendar.google.com',
+              className: 'first-year-experiance'
+          },
+        ]
+    });
+
+    $( "#master-calendar" ).click(function() {
+      $(".master-calendar").toggle();
+      $("#master-calendar").toggleClass('btn-primary btn-default');
+    });
+
+    $("#rmc-student-events").click(function() {
+      $(".rmc-student-events").toggle();
+      $("#rmc-student-events").toggleClass('btn-primary btn-default');
+    });
+
+    $("#rmc-sports").click(function() {
+      $(".rmc-sports").toggle();
+      $("#rmc-sports").toggleClass('btn-primary btn-default');
+    });
+
+    $("#first-year-experiance").click(function() {
+      $(".first-year-experiance").toggle();
+      $("#first-year-experiance").toggleClass('btn-primary btn-default');
+    });
+});
